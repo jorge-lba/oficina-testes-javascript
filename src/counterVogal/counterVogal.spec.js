b79120c592f30b06c5a4b67513ca67cc9df8f0c9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-12-15 22:58:12 -0300

    Add - Testes para o desafio para contagem de vogais

---
+++ src/counterVogal/counterVogal.spec.js
@@ -0,0 +1,32 @@
+const counterVogal = require('./index')
+
+describe("Counter Vogal", () => {
+
+  it("Deve retornar um erro casos o valor não seja uma string", () => {
+    const inputValue = 123
+    const responseExpected = "O valor não é uma string"
+
+    const result = counterVogal(inputValue)
+
+    expect(result).toBe(responseExpected)
+  })
+
+  it("Deve contar quantas vogais contem a palavra - Macarrão",() => {
+    const inputValue = "Macarrão"
+    const responseExpected = 4
+
+    const result = counterVogal(inputValue)
+
+    expect(result).toBe(responseExpected)
+  })
+
+  it("Deve contar quantas vogais contem uma frase com 109 caracteres", () => {
+    const inputValue = "Você pode encarar um erro como uma besteira a ser esquecida ou como um resultado que aponta uma nova direção."
+    const responseExpected = 47
+
+    const result = counterVogal(inputValue)
+
+    expect(result).toBe(responseExpected)
+  })
+
+})
